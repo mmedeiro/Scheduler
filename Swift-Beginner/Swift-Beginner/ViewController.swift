fd76ea1d25f2313afe1cf18b99323bcc44ef3e93
mmedeiro <31313132@mackezista.com.br>
2015-06-03 21:29:09 -0300

    part1

---
+++ Swift-Beginner/Swift-Beginner/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Swift-Beginner
+//
+//  Created by Mariana Medeiro on 03/06/15.
+//  Copyright (c) 2015 Mariana Medeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "CellTeste")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+
+
+}
+
